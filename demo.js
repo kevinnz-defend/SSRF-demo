f83b03248cd375e526f40425258b18c1d1447cb4
kevinnz-defend <123701774+kevinnz-defend@users.noreply.github.com>
2023-01-31 09:01:04 +1300

    node example working

--- demo.js
+++ demo.js
@@ -5,9 +5,13 @@ const axios = require("axios");
 const app = express();
 const port = 3000;
 
-app.get('/user/image/', async, function (req, res) {
-    const imgUrl = req.body.imgUrl;
+app.get('/user/image/', async function (req, res) {
+    const imgUrl = req.query.imgUrl;
     const imageReq = await axios.get(imgUrl);
-    user.updateProfileImage(imageReq.data);
+    //user.updateProfileImage(imageReq.data);
     res.send(imageReq.data);
 });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
--- readme.md
+++ readme.md
@@ -27,7 +27,12 @@ Enter valid URL to an image on the internet
 
 Exploit
 
-
+Python 
 ```
 $ curl -d "image=https://s3.amazonaws.com/keybase_processed_uploads/6a2d41adfced8aa30671e7fd22517d05_360_360.jpg" -X POST http://localhost/image
+```
+
+Node 
+```
+curl http://localhost:3000/user/image?imgUrl=https://s3.amazonaws.com/keybase_processed_uploads/6a2d41adfced8aa30671e7fd22517d05_360_360.jpg
 ```
